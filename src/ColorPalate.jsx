ce869596f10515ddca99c32bcc5472ff79de9dec
NiSHAN808 <nishan4b@gmail.com>
2025-03-14 22:14:45 +0545

    add color feature done

--- src/ColorPalate.jsx
+++ src/ColorPalate.jsx
@@ -1,16 +1,16 @@
-import React, { useState, lazy, useEffect } from "react";
+import React, { useState, lazy, useEffect, useRef } from "react";
 
 
-function ColorPlate({selectedColor, setSelectedColor, strokeSize, setStrokeSize }) {
+function ColorPlate({ selectedColor, setSelectedColor, strokeSize, setStrokeSize }) {
     //   selectedColor is used to return to compare  
     //    stoke size is also used in to return to compare and
     //  givve differnent css in case it is the same value that is selected 
-    
-    
+
+
     let [colors, setColors] = useState(["#41644A", "#E53888", "#443627", "#27445D", "#497D74"]);
     let [strokeList, setStrokeSizeM] = useState([2, 4, 6, 8, 10]);
 
-
+    const newColor = useRef();
 
     function handleColorClick(e, index) {
         setSelectedColor(colors[index]);
@@ -20,7 +20,10 @@ function ColorPlate({selectedColor, setSelectedColor, strokeSize, setStrokeSize 
         setStrokeSize(strokeList[index]);
     }
 
-
+    function handleAddColorClick() {
+        console.log(newColor.current.value);
+        setColors([...colors, newColor.current.value])
+    }
 
     return (
         <>
@@ -44,19 +47,22 @@ function ColorPlate({selectedColor, setSelectedColor, strokeSize, setStrokeSize 
                 }}
                     onClick={(e) => handleColorClick(e, index)}
                 >
-                
+
                 </div>)}
-                <div style={{border: "4px solid rgba(255, 255, 255, 0.45)",
-                background:"blue",
+                <div style={{
+                    border: "4px solid rgba(255, 255, 255, 0.45)",
+                    background: "blue",
                     height: "4rem",
-                    width: "2rem",
+                    width: "2rem",   // ik its terrable gonna optimize latter
                     borderRadius: "10px",
-                    marginBottom: "2px",}}>
-            <input type="color" style={{height:"1.8rem", width:"1.8rem", border:"noen"}}></input>
-            <button style={{border:"none",
-                fontSize:"1rem"
-            }}>+</button>
-            </div>
+                    marginBottom: "2px",
+                }}>
+                    <input ref={newColor} type="color" style={{ height: "1.8rem", width: "1.8rem", border: "noen" }}></input>
+                    <button onClick={handleAddColorClick} style={{
+                        border: "none",
+                        fontSize: "1rem"
+                    }}>+</button>
+                </div>
             </div>
 
             <div style={{ display: "inline-block" }} className="stroke-plate">
